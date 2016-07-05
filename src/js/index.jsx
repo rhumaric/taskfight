a2b192b72f922570f228b6010b66d3b715a5bd43
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-05 11:11:45 +0100

    Added example of autobind and static properties

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import {render} from 'react-dom';
+import autobind from 'autobind-decorator';
 
+class HelloWord extends React.Component {
+  render() {
+    return (
+      <div>Hello, {this.props.getName()}</div>
+    );
+  }
+}
+
+@autobind
 class App extends React.Component {
+
+  name="Romaric";
+
+  getName () {
+    return this.name;
+  }
+
   render () {
-    return <p> Brilliant</p>;
+    return <HelloWord getName={this.getName}/>
   }
 }
 
