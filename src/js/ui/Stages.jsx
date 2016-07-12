584cc8dd03c40d612c2b607ce44dbeb10fbfc749
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-12 16:40:55 +0100

    Updated the StagesNavigation to match expected behaviour

--- src/js/ui/Stages.jsx
+++ src/js/ui/Stages.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
 import StagesNavigation from './StagesNavigation';
+import TaskList from './TaskList';
+import Fight from './Fight';
+import Results from './Results';
 
 export default class Stages extends Component {
+
+
+
   render() {
+
     return (
 
       <div>
         {this.props.children}
-        <StagesNavigation />
+        <StagesNavigation title="TaskList" next="/fight"/>
       </div>
     );
   }
--- src/js/ui/StagesNavigation.jsx
+++ src/js/ui/StagesNavigation.jsx
@@ -4,12 +4,28 @@ import {Link} from 'react-router';
 
 export default class StagesNavigation extends Component {
 
+  renderPrevious() {
+    if (this.props.previous) {
+      return <Link to={this.props.previous}>&lt;</Link>
+    } else {
+      return <a disabled>&lt;</a>
+    }
+  }
+  renderNext() {
+    if(this.props.next) {
+      return <Link to={this.props.next}>&gt;</Link>
+    } else {
+      return <a disabled>&gt;</a>
+    }
+  }
   render() {
     return (
       <nav>
-        <Link to="tasklist">List</Link>
-        <Link to="fight">Fight</Link>
-        <Link to="results">Results</Link>
+        {this.renderPrevious()}  
+
+        <p>{this.props.title}</p>
+
+        {this.renderNext()}
       </nav>
     );
   }
