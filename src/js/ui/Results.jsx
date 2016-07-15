425fe2c11b109e9f8d4c09eccacd65041f4557ed
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-15 15:20:56 +0100

    The result page now displays the results

--- src/js/ui/Results.jsx
+++ src/js/ui/Results.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import autobind from 'autobind-decorator';
 
 export default class Results extends Component {
+
+  @autobind
+  renderResult(result) {
+
+    var task = this.props.tasks[result[0]];
+    return (
+      <li key={task.id}>{task.title}</li>
+    )
+  }
+
   render() {
     return (
-      <div>Result</div>
+      <ol>
+        {this.props.results.map(this.renderResult)}
+      </ol>
     );
   }
 }
