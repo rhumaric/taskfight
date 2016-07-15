fb25ac2efcb009a7e8de17185f0ae3357c31565d
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-15 18:38:57 +0100

    [WIP] Starting styling the app

--- src/js/ui/App.jsx
+++ src/js/ui/App.jsx
@@ -6,8 +6,9 @@ export default class App extends Component {
   render() {
     return (
       <div className="tf">
-        <header>
-          <h1>Taskfight</h1>
+        <header className="tf-Header">
+          <h1 className="visuallyhidden">Taskfight</h1>
+          <svg className="tf-Monogram" viewBox="0 0 145 125" dangerouslySetInnerHTML={{__html:"<use xlink:href='#monogram' />"}}/>
         </header>
         <main>
           <Stages {...this.props} >
