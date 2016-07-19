6c1e6c94ea4d033a1fd0bd9acf60efa2e646f5e6
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-19 15:50:19 +0100

    Adjusted styling of the list entries so they let the delete button appear correctly

--- src/js/ui/TaskListItem.jsx
+++ src/js/ui/TaskListItem.jsx
@@ -4,8 +4,8 @@ export default class TaskListItem extends Component {
   render() {
     return (
       <li className="tf-ListItem tf-TaskListItem">
-        {this.props.task.title}
-        <button className="tf-Button tf-Button-square tf-Button-transparent tf-RemoveTaskButton" 
+        <span className="tf-TaskListItem__Text">{this.props.task.title}</span>
+        <button className="tf-TaskListItem__Button tf-Button tf-Button-square tf-Button-transparent" 
                 onClick={this.props.removeTask.bind(null, this.props.task.id)}>&times;
         </button>
       </li>
