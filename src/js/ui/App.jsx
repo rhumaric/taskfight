0edfbc40828c2572ad196710a61317aa330c71dd
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-21 12:38:55 +0100

    Styled the app menu

--- src/js/ui/App.jsx
+++ src/js/ui/App.jsx
@@ -31,11 +31,13 @@ export default class App extends Component {
     return (
       <div className="tf-App">
         <header className={classnames("tf-App__Header tf-Header", {'tf-Header-noMenu': this.state.hideMenu})}>
-          <h1 className="visuallyhidden">Taskfight</h1>
-          <svg className="tf-Monogram" viewBox="0 0 145 125" dangerouslySetInnerHTML={{__html:"<use xlink:href='#monogram' />"}}/>
-          <button className="tf-MenuToggle" onClick={this.toggleMenu}>Menu</button>
+          <div className="tf-TitleBar clearfix">
+            <h1 className="visuallyhidden">Taskfight</h1>
+            <svg className="tf-Monogram" viewBox="0 0 145 125" dangerouslySetInnerHTML={{__html:"<use xlink:href='#monogram' />"}}/>
+            <button className="tf-TitleBar__MenuToggle tf-Button tf-MenuToggle" onClick={this.toggleMenu}>Menu</button>
+          </div>
           <nav className="tf-Menu tf-Header__Menu">
-            <button className="tf-MenuItem" onClick={this.confirmNewFight}>New fight</button>
+            <button className="tf-Button tf-Button-blended tf-MenuItem" onClick={this.confirmNewFight}>New fight</button>
           </nav>
         </header>
         <Stages {...this.props} className="tf-App__Main" >
