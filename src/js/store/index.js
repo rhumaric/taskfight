509143ead91873f5e01a5a982b4eb0e925595d11
Romaric Pascal <romaric.pascal@gmail.com>
2016-07-21 11:52:16 +0100

    Added menu and its toggle in the header

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -17,6 +17,7 @@ import rootReducer from './reducers';
 
 import demoState from './demoState.json';
 
+// TODO: Remove the devtools bit in production
 const store = createStore(rootReducer, demoState, compose(
   install(),
   window.devToolsExtension ? window.devToolsExtension() : f => f
--- src/js/ui/App.jsx
+++ src/js/ui/App.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
+import classnames from 'classnames';
+import autobind from 'autobind-decorator';
 
 import Stages from './Stages';
 
+@autobind
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      hideMenu: true
+    }
+  }
+
+  toggleMenu() {
+    this.setState({
+      hideMenu: !this.state.hideMenu
+    });
+  }
+
   render() {
     return (
       <div className="tf-App">
-        <header className="tf-App__Header tf-Header">
+        <header className={classnames("tf-App__Header tf-Header", {'tf-Header-noMenu': this.state.hideMenu})}>
           <h1 className="visuallyhidden">Taskfight</h1>
           <svg className="tf-Monogram" viewBox="0 0 145 125" dangerouslySetInnerHTML={{__html:"<use xlink:href='#monogram' />"}}/>
+          <button className="tf-MenuToggle" onClick={this.toggleMenu}>Menu</button>
+          <nav className="tf-Menu tf-Header__Menu">
+            <button className="tf-MenuItem">New fight</button>
+          </nav>
         </header>
         <Stages {...this.props} className="tf-App__Main" >
         {React.cloneElement(this.props.children, this.props)}
